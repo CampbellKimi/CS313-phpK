8abc6f466fa7b78da071cb94727fa831e5389256
opendoor <kimicampbell17@gmail.com>
2019-09-30 21:24:26 -0600

    added page

--- web/functions.php
+++ web/functions.php
@@ -1,4 +1,7 @@
 <?php
-echo "Why did you click that when I asked you specifically not to?\n\n\n\n\n";
-echo '<img src="carrel.gif" alt="michael scott" style="width:600px;height:800px;">';
+echo "<h1>"."NNNNOOOOOOOOOO!"."</h1>";
+echo '<img src="dwigt.gif" alt="dwight" style="width:600px;height:800px;">';
 ?>
+
+
+
