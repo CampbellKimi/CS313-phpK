83110647f080f91e76acb932c28271c3f5bcb12c
opendoor <kimicampbell17@gmail.com>
2019-10-20 18:57:24 -0600

    db

--- web/database.php
+++ web/database.php
@@ -11,15 +11,17 @@ function get_db() {
 	$db = NULL;
 	try {
 		// default Heroku Postgres configuration URL
-        $dbUrl = getenv('https://data.heroku.com/datastores/1e716ea9-f907-49b6-a4d5-e6efba9e9ffd#administration');
-        // echo $dbURL; exit;
+        $dbUrl = getenv('DATABASE_URL');
+        if (!isset($dbUrl) || empty($dbUrl)) {
+			$dbUrl = "postgres://kfwjbcqzwzpacd:14dc1474a3f84e2ac1484d56d7bf301093b255e029553e74f2ed5e1f1d7703a8@ec2-174-129-27-158.compute-1.amazonaws.com:5432/degk0pjuc8c9sm";
+		}
 		// Get the various parts of the DB Connection from the URL
 		$dbopts = parse_url($dbUrl);
-		$dbHost = $dbopts["ec2-174-129-27-158.compute-1.amazonaws.com"];
-		$dbPort = $dbopts["5432"];
-		$dbUser = $dbopts["kfwjbcqzwzpacd"];
-		$dbPassword = $dbopts["14dc1474a3f84e2ac1484d56d7bf301093b255e029553e74f2ed5e1f1d7703a8"];
-		$dbName = ltrim($dbopts["path"],'/');
+        $dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];		
+        $dbName = ltrim($dbopts["path"],'/');
 		// Create the PDO connection
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
